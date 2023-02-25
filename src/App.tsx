755dbe830fc30c54a23d15f056397164d68967a6
moritalous <moritalous@gmail.com>
2023-02-25 01:15:04 +0000

    add login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Authenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <Heading level={1}>Hello {user?.username}</Heading>
+          <Button onClick={signOut}>Sign out</Button>
+          <h2>Amplify Todos</h2>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
