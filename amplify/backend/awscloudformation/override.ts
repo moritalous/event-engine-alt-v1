2674cbe70579f4434f16e8a4989c3837455e07fd
moritalous <moritalous@gmail.com>
2023-02-25 00:50:45 +0000

    amplify override project

--- amplify/backend/awscloudformation/override.ts
+++ amplify/backend/awscloudformation/override.ts
@@ -1,5 +1,37 @@
 import { AmplifyRootStackTemplate } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyRootStackTemplate) {
-    
+
+    const authRole = resources.authRole;
+
+    const basePolicies = Array.isArray(authRole.policies)
+        ? authRole.policies
+        : [authRole.policies];
+
+    authRole.policies = [
+        ...basePolicies,
+        {
+            policyName: "amplify-permissions-custom-resources",
+            policyDocument: {
+                Version: "2012-10-17",
+                Statement: [
+                    {
+                        Resource: "*",
+                        Action: ["*"],
+                        Effect: "Allow",
+                    },
+                ],
+            },
+        },
+    ];
+
+    const baseManagedPolicy = Array.isArray(authRole.managedPolicyArns)
+    ? authRole.managedPolicyArns
+    : [authRole.managedPolicyArns];
+
+    authRole.managedPolicyArns = [
+        ...baseManagedPolicy,
+        'arn:aws:iam::aws:policy/ReadOnlyAccess'
+    ]
+
 }
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,22 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "eventenginealtbcc6920b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "eventenginealtbcc6920bPreSignup": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
