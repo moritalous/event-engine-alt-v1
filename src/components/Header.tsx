23248892d5b32924a6966824e7bb7b1339b6d83a
moritalous <moritalous@gmail.com>
2023-03-01 13:39:25 +0000

    update github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import { Divider, Flex, Heading, Link, Menu, MenuItem, View } from '@aws-amplify/ui-react';
+import { ReactComponent as GitHubMark } from '../svg/github-mark.svg'
+
 
 function Header({ signOut }: any) {
 
@@ -15,7 +17,7 @@ function Header({ signOut }: any) {
         <View marginLeft={'auto'}>
           <Flex direction={'row'}>
             <Link href='https://github.com/moritalous/event-engine-alt-v1' isExternal>
-              <img src='/github-mark.svg' width={32} height={32} alt='' />
+              <GitHubMark width={32} height={32} viewBox='0 0 98 96' />
             </Link>
             <Menu>
               <MenuItem onClick={signOut}>Sign out</MenuItem>
