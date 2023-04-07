30cc9b98c0ec0eb681164f0cd6e40bf8409b9b4f
moritalous <moritalous@users.noreply.github.com>
2023-04-08 01:49:18 +0900

    CDKをV2に更新 (#8)

--- amplify/backend/custom/customResource2b758e7dcloudwatchrum/cdk-stack.ts
+++ amplify/backend/custom/customResource2b758e7dcloudwatchrum/cdk-stack.ts
@@ -1,13 +1,13 @@
 // https://aws.amazon.com/jp/blogs/mobile/implementing-real-user-monitoring-of-amplify-application-using-amazon-cloudwatch-rum/
 
-import * as cdk from '@aws-cdk/core';
+import * as cdk from 'aws-cdk-lib';
 import * as AmplifyHelpers from '@aws-amplify/cli-extensibility-helper';
 import { AmplifyDependentResourcesAttributes } from '../../types/amplify-dependent-resources-ref';
-import * as rum from '@aws-cdk/aws-rum';
-
+import { Construct } from 'constructs';
+import * as rum from 'aws-cdk-lib/aws-rum';
 
 export class cdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps, amplifyResourceProps?: AmplifyHelpers.AmplifyResourceProps) {
     super(scope, id, props);
     /* Do not remove - Amplify CLI automatically injects the current deployment environment in this input parameter */
     new cdk.CfnParameter(this, 'env', {
